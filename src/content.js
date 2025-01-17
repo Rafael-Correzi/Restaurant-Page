1e724b31d1c76c2b1a7b995623173c55f9e6232c
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-01-17 17:09:50 -0300

    Style home page to display background image correctly

--- src/content.js
+++ src/content.js
@@ -1,19 +1,30 @@
+import imagemPizza from "./imagens/pizza.jpg";
+import svgBandeja from "./imagens/plate-svgrepo-com.svg";
+import "./styles.css"
+
 const content = document.querySelector("#content");
 
 const h1 = document.createElement("h1");
 const restaurantParagraph = document.createElement("p");
+const bandeja = document.createElement("img");
 const img = document.createElement("img");
 
 function appendHome() {
   content.appendChild(h1);
   content.appendChild(img);
+  content.appendChild(bandeja);
   content.appendChild(restaurantParagraph);
 }
 
-img.src = "";
-img.alt = "Prato de espaguete";
+img.src = imagemPizza;
+img.alt = "Pizza";
+img.classList.add("cheia");
+
+bandeja.src = svgBandeja;
+bandeja.alt = "Bandeja";
+bandeja.classList.add("bandeja");
 
 h1.textContent = "O restaurante italiano número um de São Paulo"
-restaurantParagraph.textContent = "Criado em 1955, l’idea oferece as experiência legítima da cozinha italiana. Prove os sabores inesquecíveis da Sicília, as maravilhas do Lombardia, e muita mais de toda a Itália aqui." 
+restaurantParagraph.textContent = "Prove os sabores inesquecíveis da Itália aqui." 
 
 export { content, appendHome };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { content, appendHome } from "./content.js";
 import { appendCardapio } from "./cardapio.js";
 import { appendSobre } from "./sobre.js";
 
+
 const inicio = document.querySelector("#inicio");
 const cardapio = document.querySelector("#cardapio");
 const sobre = document.querySelector("#sobre");
