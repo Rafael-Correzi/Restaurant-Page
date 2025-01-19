22e8bdcf567e6b6f8bd94fc74c294ba21b71e9d5
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-01-19 15:44:46 -0300

    Add opacity to 'cardapio', add container div to 'sobre'

--- src/sobre.js
+++ src/sobre.js
@@ -1,5 +1,6 @@
 import { content } from "./content.js"
 
+const divContainer = document.createElement("div");
 const sobre = document.createElement("h2");
 const sobreContent = document.createElement("p");
 const endereco = document.createElement("h3");
@@ -8,21 +9,24 @@ const contato = document.createElement("h3");
 const contatoEmail = document.createElement("p");
 const contatoTelefone = document.createElement("p");
 
+divContainer.classList.add("sobre-container");
 
 function appendSobre() {
-  content.appendChild(sobre);
+  content.appendChild(divContainer);
+  divContainer.appendChild(sobre);
   sobre.textContent = "Sobre nós";
-  content.appendChild(sobreContent);
-    sobreContent.textContent = "Criado em 1955, L’idea oferece as experiência legítima da cozinha italiana. Prove os sabores inesquecíveis da Sicília, as maravilhas do Lombardia, e muita mais de toda a Itália aqui."
-  content.appendChild(endereco);
+  sobreContent.textContent = "Criado em 1955, L’idea oferece as experiência legítima da cozinha italiana. \r\n" 
+  sobreContent.textContent += "Prove os sabores inesquecíveis da Sicília, as maravilhas do Lombardia, e muita mais de toda a Itália aqui."
+  divContainer.appendChild(sobreContent);
+  divContainer.appendChild(endereco);
   endereco.textContent = "Endereço";
-  content.appendChild(enderecoContent);
+  divContainer.appendChild(enderecoContent);
   enderecoContent.textContent = "Rua Xiz de Ypsilon de Souza, 111, São Paulo-SP";
-  content.appendChild(contato);
+  divContainer.appendChild(contato);
   contato.textContent = "Contato";
-  content.appendChild(contatoEmail);
+  divContainer.appendChild(contatoEmail);
   contatoEmail.textContent = "Email: email.email@email.com";
-  content.appendChild(contatoTelefone);
+  divContainer.appendChild(contatoTelefone);
   contatoTelefone.textContent = "Telefone: (11)99999-9999";
 }
 
