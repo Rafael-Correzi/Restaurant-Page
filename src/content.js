4a609baf05d9bef245b4fd9dd14324b70b292129
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-01-19 19:17:19 -0300

    Attribute credits to pizza image

--- src/content.js
+++ src/content.js
@@ -9,17 +9,24 @@ const p = create("p");
 setContent(p, "L'idea");
 p.classList.add("logo");
 
-
+const figure = create("figure");
 const img = create("img");
+const figCaption = create("figcaption");
+
 
 function addContent() { 
-  content.appendChild(img);
+  content.appendChild(figure);
+  figure.appendChild(img);
+  figure.appendChild(figCaption);
   logoContainer.appendChild(p);
 }
 
+figure.classList.add("figure-cheia");
 img.src = imagemPizza;
 img.alt = "Pizza";
 img.classList.add("cheia");
+figCaption.innerHTML = 'Photo by <a href="https://unsplash.com/@amir_v_ali?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">amirali mirhashemian</a> on <a href="https://unsplash.com/photos/pizza-with-green-leaves-and-red-sauce-XtLPfib7OuM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>'
+      
 
 addContent();
 
